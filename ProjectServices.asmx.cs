fbba6b1d07948e8ea0c28d9e5718542c98a13d59
jasabbag-asu <60336789+jasabbag-asu@users.noreply.github.com>
2020-02-24 18:10:47 -0700

    Add files via upload

---
+++ ProjectServices.asmx.cs
@@ -0,0 +1,110 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ProjectTemplate
+{
+	[WebService(Namespace = "http://tempuri.org/")]
+	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+	[System.ComponentModel.ToolboxItem(false)]
+	[System.Web.Script.Services.ScriptService]
+
+	public class ProjectServices : System.Web.Services.WebService
+	{
+		////////////////////////////////////////////////////////////////////////
+		///replace the values of these variables with your database credentials
+		////////////////////////////////////////////////////////////////////////
+		private string dbID = "weavers";
+		private string dbPass = "!!Weavers";
+		private string dbName = "weavers";
+		////////////////////////////////////////////////////////////////////////
+		
+		////////////////////////////////////////////////////////////////////////
+		///call this method anywhere that you need the connection string!
+		////////////////////////////////////////////////////////////////////////
+		private string getConString() {
+			return "SERVER=107.180.1.16; PORT=3306; DATABASE=" + dbName+"; UID=" + dbID + "; PASSWORD=" + dbPass;
+
+        }
+		////////////////////////////////////////////////////////////////////////
+
+
+
+		/////////////////////////////////////////////////////////////////////////
+		//don't forget to include this decoration above each method that you want
+		//to be exposed as a web service!
+		[WebMethod(EnableSession = true)]
+		/////////////////////////////////////////////////////////////////////////
+		public string TestConnection()
+		{
+			try
+			{
+				string testQuery = "select * from Users";
+
+				////////////////////////////////////////////////////////////////////////
+				///here's an example of using the getConString method!
+				////////////////////////////////////////////////////////////////////////
+				MySqlConnection con = new MySqlConnection(getConString());
+				////////////////////////////////////////////////////////////////////////
+
+				MySqlCommand cmd = new MySqlCommand(testQuery, con);
+				MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+				DataTable table = new DataTable();
+				adapter.Fill(table);
+				return "Success!";
+			}
+			catch (Exception e)
+			{
+				return "Something went wrong, please check your credentials and db name and try again.  Error: "+e.Message;
+			}
+		}
+        [WebMethod(EnableSession = true)] //NOTICE: gotta enable session on each individual method
+        public bool LogOn(string uid, string pass)
+        {
+            //we return this flag to tell them if they logged in or not
+            bool success = false;
+
+            //our connection string comes from our web.config file like we talked about earlier
+            string sqlConnectString = "SERVER=107.180.1.16; PORT=3306; DATABASE=" + dbName + "; UID=" + dbID + "; PASSWORD=" + dbPass;
+            //System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+            //here's our query.  A basic select with nothing fancy.  Note the parameters that begin with @
+            //NOTICE: we added admin to what we pull, so that we can store it along with the id in the session
+            string sqlSelect = "SELECT * FROM Users WHERE username=@idvalue and password=@passvalue";
+
+            //set up our connection object to be ready to use our connection string
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            //set up our command object to use our connection, and our query
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            //tell our command to replace the @parameters with real values
+            //we decode them because they came to us via the web so they were encoded
+            //for transmission (funky characters escaped, mostly)
+            sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(uid));
+            sqlCommand.Parameters.AddWithValue("@passValue", HttpUtility.UrlDecode(pass));
+
+            //a data adapter acts like a bridge between our command object and 
+            //the data we are trying to get back and put in a table object
+            MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+            //here's the table we want to fill with the results from our query
+            DataTable sqlDt = new DataTable();
+            //here we go filling it!
+            sqlDa.Fill(sqlDt);
+            //check to see if any rows were returned.  If they were, it means it's 
+            //a legit account
+            if (sqlDt.Rows.Count > 0)
+            {
+              success = true;
+            }
+            //return the result!
+            return success;
+        }
+
+
+    }
+}
